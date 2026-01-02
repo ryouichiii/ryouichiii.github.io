97e7942036db1362f88f5663a3269e733057167d
ryouichi <150134782+ryouichiii@users.noreply.github.com>
2026-01-02 09:39:39 +0000

    Fully functional link carousel in footer, now implemented in every page

---
+++ links.js
@@ -0,0 +1,51 @@
+/* i hate copy and pasting so i shoved the footer contents into a javascript, sue me */
+document.write(`
+<!-- peeslubn -->
+<div class="carousel-image item1">
+    <a href="https://peeslubn.neocities.org/">
+        <img src="./images/carousel/peeslubn.gif" alt="peeslubn">
+    </a>
+</div>
+<!-- peeslubn -->
+<div class="carousel-image item2">
+    <a href="https://peeslubn.neocities.org/">
+        <img src="./images/carousel/peeslubn.gif" alt="peeslubn">
+    </a>
+</div>
+<!-- peeslubn -->
+<div class="carousel-image item3">
+    <a href="https://peeslubn.neocities.org/">
+        <img src="./images/carousel/peeslubn.gif" alt="peeslubn">
+    </a>
+</div>
+<!-- peeslubn -->
+<div class="carousel-image item4">
+    <a href="https://peeslubn.neocities.org/">
+        <img src="./images/carousel/peeslubn.gif" alt="peeslubn">
+    </a>
+</div>
+<!-- peeslubn -->
+<div class="carousel-image item5">
+    <a href="https://peeslubn.neocities.org/">
+        <img src="./images/carousel/peeslubn.gif" alt="peeslubn">
+    </a>
+</div>
+<!-- peeslubn -->
+<div class="carousel-image item6">
+    <a href="https://peeslubn.neocities.org/">
+        <img src="./images/carousel/peeslubn.gif" alt="peeslubn">
+    </a>
+</div>
+<!-- peeslubn -->
+<div class="carousel-image item7">
+    <a href="https://peeslubn.neocities.org/">
+        <img src="./images/carousel/peeslubn.gif" alt="peeslubn">
+    </a>
+</div>
+<!-- peeslubn -->
+<div class="carousel-image item8">
+    <a href="https://peeslubn.neocities.org/">
+        <img src="./images/carousel/peeslubn.gif" alt="peeslubn">
+    </a>
+</div>
+`);
