f26886297c16a7d7fae10fe602c8eede8f96b550
ryouichi <150134782+ryouichiii@users.noreply.github.com>
2026-01-02 20:41:08 +0000

    Added a whole bunch of links to carousel

--- links.js
+++ links.js
@@ -6,46 +6,202 @@ document.write(`
         <img src="./images/carousel/peeslubn.gif" alt="peeslubn">
     </a>
 </div>
-<!-- peeslubn -->
+<!-- haku -->
 <div class="carousel-image item2">
-    <a href="https://peeslubn.neocities.org/">
-        <img src="./images/carousel/peeslubn.gif" alt="peeslubn">
+    <a href="https://hqpng.art/">
+        <img src="./images/carousel/haku.webp" alt="hq (haku)">
     </a>
 </div>
-<!-- peeslubn -->
+<!-- neku -->
 <div class="carousel-image item3">
-    <a href="https://peeslubn.neocities.org/">
-        <img src="./images/carousel/peeslubn.gif" alt="peeslubn">
+    <a href="https://neku.uwu.ai/">
+        <img src="./images/carousel/neku.webp" alt="neku">
     </a>
 </div>
-<!-- peeslubn -->
+<!-- meowbait -->
 <div class="carousel-image item4">
-    <a href="https://peeslubn.neocities.org/">
-        <img src="./images/carousel/peeslubn.gif" alt="peeslubn">
+    <a href="https://meowbait.carrd.co/">
+        <img src="./images/carousel/meowbait.webp" alt="meowbait">
     </a>
 </div>
-<!-- peeslubn -->
+<!-- pann -->
 <div class="carousel-image item5">
-    <a href="https://peeslubn.neocities.org/">
-        <img src="./images/carousel/peeslubn.gif" alt="peeslubn">
+    <a href="https://vocapann.info/">
+        <img src="./images/carousel/pann.webp" alt="panncake">
     </a>
 </div>
-<!-- peeslubn -->
+<!-- fnv -->
 <div class="carousel-image item6">
-    <a href="https://peeslubn.neocities.org/">
-        <img src="./images/carousel/peeslubn.gif" alt="peeslubn">
+    <a href="https://fnv-utau.carrd.co/">
+        <img src="./images/carousel/fnv.gif" alt="FNV">
     </a>
 </div>
-<!-- peeslubn -->
+<!-- otori -->
 <div class="carousel-image item7">
-    <a href="https://peeslubn.neocities.org/">
-        <img src="./images/carousel/peeslubn.gif" alt="peeslubn">
+    <a href="https://otori-san-net.carrd.co/">
+        <img src="./images/carousel/otori.webp" alt="peeslubn">
     </a>
 </div>
-<!-- peeslubn -->
+<!-- dayton -->
 <div class="carousel-image item8">
-    <a href="https://peeslubn.neocities.org/">
-        <img src="./images/carousel/peeslubn.gif" alt="peeslubn">
+    <a href="https://singingpanda.uwu.ai/">
+        <img src="./images/carousel/dayton.webp" alt="dayton">
+    </a>
+</div>
+<!-- liure -->
+<div class="carousel-image item9">
+    <a href="https://sites.google.com/view/shin-liure/home">
+        <img src="./images/carousel/liure.webp" alt="liure">
+    </a>
+</div>
+<!-- nebm -->
+<div class="carousel-image item10">
+    <a href="https://nebulameadow.com/#home">
+        <img src="./images/carousel/nebm.webp" alt="nebm">
+    </a>
+</div>
+<!-- tasteloid -->
+<div class="carousel-image item11">
+    <a href="https://www.tasteloid.com/">
+        <img src="./images/carousel/tasteloid.webp" alt="nebm">
+    </a>
+</div>
+<!-- utauraptor -->
+<div class="carousel-image item12">
+    <a href="https://utauraptor.carrd.co/">
+        <img src="./images/carousel/utauraptor.webp" alt="utauraptor">
+    </a>
+</div>
+<!-- joezcafe -->
+<div class="carousel-image item13">
+    <a href="https://linktr.ee/joezcafe">
+        <img src="./images/carousel/joezcafe.webp" alt="joezcafe">
+    </a>
+</div>
+<!-- keitaiware -->
+<div class="carousel-image item14">
+    <a href="https://keitaiware.com/">
+        <img src="./images/carousel/keitaiware.webp" alt="keitaiware">
+    </a>
+</div>
+<!-- hetatan -->
+<div class="carousel-image item15">
+    <a href="https://hetatancommissions.wordpress.com/">
+        <img src="./images/carousel/hetatan.webp" alt="hetatan">
+    </a>
+</div>
+<!-- vocaloader -->
+<div class="carousel-image item16">
+    <a href="https://vocaloader.app/">
+        <img src="./images/carousel/vocaloader.webp" alt="vocaloader">
+    </a>
+</div>
+<!-- lunai -->
+<div class="carousel-image item17">
+    <a href="https://lunaiproject.github.io/">
+        <img src="./images/carousel/lunai.webp" alt="lunai">
+    </a>
+</div>
+<!-- ufr -->
+<div class="carousel-image item18">
+    <a href="https://utaufrance.com/">
+        <img src="./images/carousel/ufr.webp" alt="ufr">
+    </a>
+</div>
+<!-- idoloid -->
+<div class="carousel-image item19">
+    <a href="https://www.idoloid.us/">
+        <img src="./images/carousel/idoloid.webp" alt="idoloid">
+    </a>
+</div>
+<!-- mackhouse -->
+<div class="carousel-image item20">
+    <a href="https://www.mackhouse-soft.com/">
+        <img src="./images/carousel/mackne.webp" alt="mackhousesoft">
+    </a>
+</div>
+<!-- neodiva -->
+<div class="carousel-image item21">
+    <a href="https://neodiva.xyz/#home">
+        <img src="./images/carousel/neodiva.webp" alt="neodiva">
+    </a>
+</div>
+<!-- tigermeat -->
+<div class="carousel-image item22">
+    <a href="https://tigermeat.xyz/">
+        <img src="./images/carousel/tigermeat.webp" alt="tigermeat">
+    </a>
+</div>
+<!-- ryan-kun -->
+<div class="carousel-image item23">
+    <a href="https://ryankuncomms.carrd.co/">
+        <img src="./images/carousel/ryan.webp" alt="ryan-kun">
+    </a>
+</div>
+<!-- ubloid -->
+<div class="carousel-image item24">
+    <a href="https://projectubloid.neocities.org/">
+        <img src="./images/carousel/ubloid.webp" alt="ubloid">
+    </a>
+</div>
+<!-- lunarconstruct -->
+<div class="carousel-image item25">
+    <a href="https://lunarconstruct.net/vocalsynth/">
+        <img src="./images/carousel/lunarconstruct.webp" alt="lunarconstruct">
+    </a>
+</div>
+<!-- potg -->
+<div class="carousel-image item26">
+    <a href="https://potgutau.creatorlink.net/">
+        <img src="./images/carousel/potg.webp" alt="potg">
+    </a>
+</div>
+<!-- xepheris -->
+<div class="carousel-image item27">
+    <a href="https://xepheris.wixsite.com/milk">
+        <img src="./images/carousel/xepheris.webp" alt="xepheris">
+    </a>
+</div>
+<!-- haraao -->
+<div class="carousel-image item28">
+    <a href="https://haraaoutau.nekoweb.org/">
+        <img src="./images/carousel/haraao.webp" alt="haraao">
+    </a>
+</div>
+<!-- mystsaphyr -->
+<div class="carousel-image item29">
+    <a href="https://mystsaphyr.art/">
+        <img src="./images/carousel/myst.gif" alt="mystsaphyr">
+    </a>
+</div>
+<!-- six -->
+<div class="carousel-image item30">
+    <a href="https://sixlightyears.com/">
+        <img src="./images/carousel/six.webp" alt="six">
+    </a>
+</div>
+<!-- shinnosuke -->
+<div class="carousel-image item31">
+    <a href="https://betweenportals.wixsite.com/utau-voicebank/shin-nosuke">
+        <img src="./images/carousel/shinnosuke.webp" alt="shinnosuke">
+    </a>
+</div>
+<!-- gyutaeng -->
+<div class="carousel-image item32">
+    <a href="https://09330528.wixsite.com/vocal">
+        <img src="./images/carousel/gyutaeng.webp" alt="gyutaeng">
+    </a>
+</div>
+<!-- konagi -->
+<div class="carousel-image item33">
+    <a href="https://numanoie.jimdofree.com/">
+        <img src="./images/carousel/konagi.webp" alt="konagi">
+    </a>
+</div>
+<!-- ttd -->
+<div class="carousel-image item34">
+    <a href="https://text-to-death.carrd.co/">
+        <img src="./images/carousel/ttd.webp" alt="ttd">
     </a>
 </div>
 `);
